e7586677906ab08fe50eaf8c3fa9d689e82dc48f
Markos Lerin <marcoslerin@gmail.com>
2017-10-13 14:37:46 -0500

    Adding spring profiling

--- src/main/java/com/marcos/springboot/controller/UserController.java
+++ src/main/java/com/marcos/springboot/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.marcos.springboot.controller;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.marcos.springboot.entities.Role;
 import com.marcos.springboot.entities.User;
 import com.marcos.springboot.service.UserService;
 
@@ -20,21 +17,10 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping(value="/user", method=RequestMethod.POST)
-	public User saveUser() {		
-		User user = new User();
-		user.setPassword("123");
-		user.setPasswordConfirm("123");
-		user.setUsername("markos");
-		
-		Set<Role> roles = new HashSet<>();
-		Role role = new Role();
-		role.setId(1L);
-		roles.add(role);
-		user.setRoles(roles);
-		
+	public User saveUser(@RequestBody User user) {		
 		return userService.saveUser(user);
 	}
-	
+			
 	@RequestMapping(value="/user/{id}", method=RequestMethod.GET)
 	public User getUser(@PathVariable Long id) {		
 		return userService.findById(id);
